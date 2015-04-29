863b735ca7f63a6643bc2c7f60d8409cc8173bd6
Jon SP <jonsp14@gmail.com>
2015-04-29 19:16:50 -0400

    Solr addition

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 var express = require('express');
+var parser = require("body-parser");
 var app = express();
+app.use(parser.json())
+var solr = require('solr-client');
+var client = solr.createClient({
+	
+});
+client.autoCommit = true;
+
+var uuid = require("uuid");
+
+app.post('/advisor', function (req, res){
+
+	var input = req.body;
+	if(!input.id){
+		input.id = uuid.v4();
+	}
+
+	client.add(input, function(err,obj){
+		if(err){
+			console.log(err);
+		}
+		else{
+			console.log('Solr response:', obj);
+			client.commit();
+		}
+	})
+
+	res.send("ok");
+});
+
+
 
 app.get('/', function (req, res) {
 	var result = {
@@ -7,7 +38,7 @@ app.get('/', function (req, res) {
 		date : new Date(),
 	}
   res.send(result);
-});
+}); 
 
 var server = app.listen(3000, function () {
 
