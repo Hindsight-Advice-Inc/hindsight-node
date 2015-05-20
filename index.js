9948af2cd44c8c154f5b4fe4f1b9ddc421b6e3c4
Jon SP <jonsp14@gmail.com>
2015-05-19 23:35:38 -0400

    neo4j updates

--- index.js
+++ index.js
@@ -6,22 +6,7 @@ app.use(parser.json())
 
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://neo-2.ironbay.digital');
-console.log(db)
-db.cypher({
-    query: 'MATCH (u:User {email: {email}}) RETURN u',
-    params: {
-        email: 'alice@example.com',
-    },
-}, function (err, results) {
-    if (err) throw err;
-    var result = results[0];
-    if (!result) {
-        console.log('No user found.');
-    } else {
-        var user = result['u'];
-        console.log(JSON.stringify(user, null, 4));
-    }
-});
+
 
 var solr = require('solr-client');
 var client = solr.createClient({
@@ -32,96 +17,68 @@ client.autoCommit = true;
 
 var uuid = require("uuid");
 
-app.post('/advisor', function (req, res){
-
+app.post('/me', function (req, res){
 	var input = req.body;
 	if(!input.id){
-		input.id = uuid.v4();
+		//input.id = uuid.v4();
+		input.id = "123";
 	}
-
-	client.add(input, function(err,obj){
-		if(err){
-			res.send(err)
-		}
-		else{
-			client.commit();
-		}
-
-		res.send(input);
-	})
-
+	console.log(input)
+	db.cypher({
+    	query: 'MERGE (n:User {id :{id} }) SET n.email = {email}, n.name = {name}',
+    	params: {
+        	id: input.id,
+        	email: input.email,
+        	name: input.name,
+    	},
+	}, function (err, results) {
+	    if (err) throw err;
+	    res.send(input);
+	});
 });
 
-app.post('/auth/login', function (req, res){
+app.post("/me/request/:user", function(req, res) {
 
+	db.cypher({
+    	query: 
+    		'MATCH (me:User { id : {me} } ), (target:User {id: {tid}} ) ' +
+    		'MERGE (r:Request { id : {rid}, paid : false, accepted : false }) ' + 
+    		'CREATE UNIQUE (me)-[:CREATE_REQUEST]->(r)<-[:HAS_REQUEST]-(target)',
+    	params: {
+        	rid: uuid.v4(),
+        	me : "123",
+        	tid : "456"
+    	},
+	}, function (err, results) {
+	    if (err) throw err;
+	    res.send("ok");
+	});
+});
 
-})
+app.post("/me/accept/:request", function(req, res){
 
-app.post('/auth/logout', function (req, res){
-
-
-})
-
-app.get('/me', function (req, res){
-
-	
-})
-
-app.get('/advisor/search', function (req, res){
-
-	var query = client.createQuery().q(req.query.query);
-	console.log(query)
-	client.search(query,function(err,obj){
-		if (err) {
-			res.send(err);
-			return;
+	db.cypher({
+		query: 'MATCH (r:Request {id : {rid}}) SET r.accepted = true',
+		params : {
+			rid : "bf7ff0a4-e067-46b1-9c5e-c189e92ce848"
 		}
-		res.send(obj.response.docs);
-	})
-})
-
-app.get('/advisor/:id', function (req, res){
-	var id = req.param('id');
-	var query = client.createQuery().q('id:' + id);
-	client.search(query,function(err, obj){
-		res.send(obj.response.docs[0]);
-
+	}, function (err, results) {
+		if(err) throw err;
+		res.send("ok")
 	})
 })
 
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
-});
-var express = require('express');
-var parser = require("body-parser");
-var app = express();
-app.use(parser.json())
-
-
-var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://neo-2.ironbay.digital');
-var node = db.createNode({hello: 'world'});     // instantaneous, but...
-node.save(function (err, node) {    // ...this is what actually persists.
-    if (err) {
-        console.error('Error saving new node to database:', err);
-    } else {
-        console.log('Node saved to database with id:', node.id);
-    }
-});
-
-var solr = require('solr-client');
-var client = solr.createClient({
-	
-});
-client.autoCommit = true;
-
-
-var uuid = require("uuid");
+app.post("/me/pay/:request", function(req, res){
+	db.cypher({
+		query: 'MATCH (r:Request {id : {rid}}) SET r.paid = true',
+		params : {
+			rid : "bf7ff0a4-e067-46b1-9c5e-c189e92ce848"
+		}
+	}, function (err, results) {
+		if(err) throw err;
+		res.send("ok")
+	})
+})
 
 app.post('/advisor', function (req, res){
 
