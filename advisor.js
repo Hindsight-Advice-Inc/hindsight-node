a82279ffdeb847cb1fd8cee466865a6e3d6a0f0a
Dax Ravi <d@ironbay.digital>
2015-05-31 21:13:15 +0000

    Advisor endpoint

--- advisor.js
+++ advisor.js
@@ -20,10 +20,10 @@ module.exports = function(app) {
 		var id = req.query.id;
 		neo.cypher({
 			query : 
-				"MATCH (u:User { id : '456'} ), " + 
-				"(u)-[ti:HAS_TEST]-(t:Test), " +
-				"(u)-[si:HAS_SCHOOL]-(s:SCHOOL), " +
-				"(u)-[ei:HAS_EMPLOYER]-(e:Employer) " +
+				"MATCH (u:User { id : '456'} ) " + 
+				"OPTIONAL MATCH (u)-[ti:HAS_TEST]-(t:Test) " +
+				"OPTIONAL MATCH (u)-[si:HAS_SCHOOL]-(s:SCHOOL) " +
+				"OPTIONAL MATCH (u)-[ei:HAS_EMPLOYER]-(e:Employer) " +
 				"return u, collect(ti) as testInfo, collect(t) as test, collect(si) as schoolInfo, collect(s) as school, collect(ei) as employerInfo, collect(e) as employer"
 		}, function(err, results) {
 			if(err) {
@@ -33,18 +33,20 @@ module.exports = function(app) {
 			}
 
 			var result = results[0]
-			console.log(results)
 			var payload = {};
 			payload.user = {
 				name : result.u.properties.name,
 				story : result.u.properties.story
 			}
 
-			["test", "employer", "school"].forEach(function(field) {
+			var fields = ["test", "employer", "school"];
+
+			fields.forEach(function(field, i) {
 				payload[field] = [];
 				result[field].forEach(function(obj,i) {
 					var match = result[field + "Info"][i].properties;
 					match[field] = obj.properties.id;
+					payload.type = field;
 					payload[field].push(match);
 				})
 			})
