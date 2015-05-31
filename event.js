14621c689876ff200c799dfc96bb16509062035e
Dax Ravi <d@ironbay.digital>
2015-05-31 20:36:44 +0000

    Proper error handling

--- event.js
+++ event.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
 		}, function(err, results) {
 			if(err) {
 				res.status(500);
-				res.send();
+				res.send(err);
 				return;
 			}
 
@@ -42,7 +42,7 @@ module.exports = function(app) {
 		}, function(err, results) {
 			if(err) {
 				res.status(500);
-				res.send();
+				res.send(err);
 				return;
 			}
 
@@ -66,7 +66,7 @@ module.exports = function(app) {
 		}, function(err, results) {
 			if(err) {
 				res.status(500);
-				res.send();
+				res.send(err);
 				return;
 			}
 
